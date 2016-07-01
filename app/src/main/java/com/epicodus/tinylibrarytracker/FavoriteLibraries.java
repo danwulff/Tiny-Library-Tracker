00e246d78a6d9c27afadd25e7cc8ee571614a02c
Daniel Wulff <danielwulff.ee@gmail.com>
2016-07-01 09:11:46 -0700

    created activities, color theme and up buttons

---
+++ app/src/main/java/com/epicodus/tinylibrarytracker/FavoriteLibraries.java
@@ -0,0 +1,13 @@
+package com.epicodus.tinylibrarytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoriteLibraries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_libraries);
+    }
+}
---
+++ app/src/main/java/com/epicodus/tinylibrarytracker/SearchResults.java
@@ -0,0 +1,13 @@
+package com.epicodus.tinylibrarytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+}
