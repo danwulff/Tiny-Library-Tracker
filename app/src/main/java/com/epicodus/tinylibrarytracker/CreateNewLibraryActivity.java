2ff752ae6eba9ad9db0fb2df193c6517a938259e
Daniel Wulff <danielwulff.ee@gmail.com>
2016-07-10 14:39:41 -0700

    prettied up the create new library activity

--- app/src/main/java/com/epicodus/tinylibrarytracker/CreateNewLibraryActivity.java
+++ app/src/main/java/com/epicodus/tinylibrarytracker/CreateNewLibraryActivity.java
@@ -1,13 +1,33 @@
 package com.epicodus.tinylibrarytracker;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class CreateNewLibraryActivity extends AppCompatActivity {
+    @Bind(R.id.backgroundLayout) View mBackgroundLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_library);
+        ButterKnife.bind(this);
+
+        View backgroundimage = mBackgroundLayout;
+        Drawable background = backgroundimage.getBackground();
+        background.setAlpha(80);
     }
 }
