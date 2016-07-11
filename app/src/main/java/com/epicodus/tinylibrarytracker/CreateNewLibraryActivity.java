0eb232fe169a839c962e5c7bdf06cd29e22784c7
Daniel Wulff <danielwulff.ee@gmail.com>
2016-07-11 00:27:26 -0700

    broken code, trying to save a photo to the gallery, but can't get it to appear. Commit as reference, going to try a different route

--- app/src/main/java/com/epicodus/tinylibrarytracker/CreateNewLibraryActivity.java
+++ app/src/main/java/com/epicodus/tinylibrarytracker/CreateNewLibraryActivity.java
@@ -8,7 +8,10 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +24,14 @@ import android.widget.RelativeLayout;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
@@ -30,6 +41,7 @@ public class CreateNewLibraryActivity extends AppCompatActivity {
     @Bind(R.id.placeholderImage) ImageView mPlaceholderImage;
 
     public static final String TAG = CreateNewLibraryActivity.class.getSimpleName();
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     @Override
@@ -61,8 +73,7 @@ public class CreateNewLibraryActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int item) {
                 if (items[item].equals("Take Photo")) {
                     Log.d(TAG, "take photo chosen");
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent, 0);
+                    dispatchTakePictureIntent();
                 } else if (items[item].equals("Choose from Library")) {
                     Log.d(TAG, "choose from library chosen");
                 } else if (items[item].equals("Cancel")) {
@@ -73,24 +84,63 @@ public class CreateNewLibraryActivity extends AppCompatActivity {
         builder.show();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    String mCurrentPhotoPath;
 
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == 0) {
-                Log.d(TAG, "take photo result");
-                takePhotoResult(data);
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_tinylibrary";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
             }
-            else if (requestCode == 1) {
-                Log.d(TAG, "choose from library result");
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.epicodus.android.tinylibrarytracker",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
     }
 
-    private void takePhotoResult(Intent data) {
-        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-        mPlaceholderImage.setImageBitmap(thumbnail);
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            mPlaceholderImage.setImageBitmap(imageBitmap);
+//        }
+    }
 }
